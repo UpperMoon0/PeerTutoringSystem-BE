194749ae7ced272786dd3c8cdf436dc655e8a496
Vo Nguyen Minh Nhat <60876500+UpperMoon0@users.noreply.github.com>
2025-08-16 19:44:41 +0700

    update: payment list

--- PeerTutoringSystem.Application/DTOs/Payment/PaymentDto.cs
+++ PeerTutoringSystem.Application/DTOs/Payment/PaymentDto.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 namespace PeerTutoringSystem.Application.DTOs.Payment
 {
-    public class PaymentHistoryDto
+    public class PaymentDto
     {
         public Guid Id { get; set; }
+        public string TransactionId { get; set; }
+        public Guid BookingId { get; set; }
         public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime TransactionDate { get; set; }
         public string Status { get; set; }
         public string TutorName { get; set; }
         public string StudentName { get; set; }
--- PeerTutoringSystem.Application/Interfaces/Payment/IPaymentService.cs
+++ PeerTutoringSystem.Application/Interfaces/Payment/IPaymentService.cs
@@ -5,12 +5,13 @@ namespace PeerTutoringSystem.Application.Interfaces.Payment
 {
     public interface IPaymentService
     {
-        Task<IEnumerable<PaymentHistoryDto>> GetPaymentHistory(string userId);
+        Task<IEnumerable<PaymentDto>> GetPaymentHistory(string userId);
         Task<PayOSCreatePaymentLinkResponseDto> CreatePaymentLink(PayOSCreatePaymentLinkRequestDto request);
         Task<bool> ConfirmPayment(Guid bookingId);
         Task HandlePayOSWebhook(PayOSWebhookData webhookData);
         Task<BookingSessionDto> HandlePayOSReturn(long orderCode);
         Task<BookingSessionDto> HandlePayOSCancel(long orderCode);
         Task<FinanceDetailsDto> GetFinanceDetails();
+        Task<IEnumerable<PaymentDto>> GetAllPayments();
     }
 }
